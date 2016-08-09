82a76323aceac4eba5b32d69b2134a743989086f
tchungnz <tchung.nz@gmail.com>
2016-08-09 12:12:12 +0100

    Install mocha and chai

---
+++ models/space.js
@@ -0,0 +1,5 @@
+// Space = function(price,name,description) {
+//   this.price = price;
+//   this.name = name;
+//   this.description = description;
+// };
---
+++ spec/spaceSpec.js
@@ -0,0 +1,20 @@
+// var expect = require('chai').expect;
+// var loadSpace = require('../models/space');
+//
+// describe('space', function() {
+//
+//   var space;
+//
+//   beforeEach(function() {
+//     space = new Space();
+//   });
+//
+//   describe('initialise/constructor', function() {
+//     it('when constructed has a name, description and price', function() {
+//       expect(space).to.have.property(price);
+//       expect(space).to.have.property(name);
+//       expect(space).to.have.property(description);
+//     });
+//   });
+//
+// });
