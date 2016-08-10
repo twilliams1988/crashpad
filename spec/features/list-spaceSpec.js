3fe3e615595daefeb17a3637b7484723d0d4dbe1
Terry <terry@terrytilley.com>
2016-08-10 10:57:52 +0100

    Updated tests and fixed typo on sign up page

--- spec/features/list-spaceSpec.js
+++ spec/features/list-spaceSpec.js
@@ -3,21 +3,22 @@ var Browser = require('zombie');
 
 Browser.localhost('example.com', 3000);
 
-describe('User visits list space page', function() {
+describe('User visits list pad page', function() {
 
   browser = new Browser();
   var url = 'http://localhost:3000';
 
   before(function(done) {
-    browser.visit(url + '/spaces/new', done);
+    browser.visit(url + '/pads/new', done);
   });
 
-  describe('list space', function() {
+  describe('list pad', function() {
 
     before(function(done) {
       browser
         .fill('name',         'Terry\'s House')
         .fill('description',  'This is a description')
+        .fill('location',     'London')
         .fill('price',        200)
         .pressButton('List my pad', done);
     });
@@ -26,8 +27,8 @@ describe('User visits list space page', function() {
       browser.assert.success();
     });
 
-    it('will redirect to /spaces', function() {
-      browser.assert.url(url + '/spaces');
+    it('will redirect to /pads', function() {
+      browser.assert.url(url + '/pads');
     });
 
   });
